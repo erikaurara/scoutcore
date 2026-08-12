88c22d9b64a04473b791b732838291f862012b88
erikaurara <uraya1207@gmail.com>
2026-08-13 03:27:49 +0900

    [skip netlify] load live matchup animation styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './onboarding.css';
 import './account-polish.css';
+import './live-matchup.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
