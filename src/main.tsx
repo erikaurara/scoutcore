c7adef14c00cf840351dc732849816312278255e
erikaurara <uraya1207@gmail.com>
2026-08-14 02:21:25 +0900

    [skip ci] [skip netlify] wrap ScoutCore with language provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { LanguageProvider } from './i18n/LanguageContext';
 import './index.css';
 import './onboarding.css';
 import './account-polish.css';
@@ -12,6 +13,8 @@ import './readability.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 );
