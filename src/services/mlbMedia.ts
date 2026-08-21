f69954a7d443248be7ef288c96c908cc2e6e34f8
erikaurara <uraya1207@gmail.com>
2026-08-22 04:27:27 +0900

    feat: add transparent MLB player cutouts

--- src/services/mlbMedia.ts
+++ src/services/mlbMedia.ts
@@ -6,6 +6,11 @@ export const mlbPlayerHeadshotUrl = (playerId?: number | null, width = 180) =>
     ? `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:silo:current.png/w_${width},q_auto:best/v1/people/${playerId}/headshot/67/current`
     : '';
 
+export const mlbPlayerCutoutUrl = (playerId?: number | null, width = 180) =>
+  playerId
+    ? `https://img.mlbstatic.com/mlb-photos/image/upload/e_background_removal,f_png,w_${width},q_auto:best/v1/people/${playerId}/headshot/67/current`
+    : '';
+
 export const playerInitials = (name?: string) =>
   (name ?? 'MLB')
     .split(/\s+/)
